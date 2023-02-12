e565ef9041d35dff1c49a59fd5c00f5ce929f9a1
smeyanoff <miradg99@gmail.com>
2023-02-12 20:41:32 +0400

    get embanndings

--- config/config.py
+++ config/config.py
@@ -1,6 +1,13 @@
 import os
 
+import torch
+
 class Config(object):
 
     WEIGHTS_PATH = os.path.normpath('model/weights')
     MODEL_NAME = 'vggface2'
+    
+    # if torch.cuda.is_available():
+    #     DEVICE = "cuda"
+    # else:
+    DEVICE = "cpu"
--- main.py
+++ main.py
@@ -3,18 +3,33 @@ import torchvision
 from config.config import Config
 from model import res_net
 
+from config.config import Config
+
 if __name__ == '__main__':
+
+    config = Config()
     
     model = res_net.InceptionResnetV1(
         pretrained = True,
         dropout_prob=0.6, 
-        device=None
+        device=config.DEVICE
     )
-    model.eval()
+    model.eval().to(config.DEVICE)
     
-    photo_path = "data/dima/photo_2023-02-12_16-10-37.jpg"
+    photo_path = "data/dima/"
+    photo1 = photo_path+"photo3.jpg"
+    photo2 = photo_path+"photo4.jpg"
 
-    photo = torchvision.io.read_image(photo_path)
-    embanding = model.forward(photo)
 
-    print(embanding)
+    photo1 = torchvision.io.read_image(photo1)
+    photo1 = photo1.float().unsqueeze(0).to(config.DEVICE)
+
+    photo2 = torchvision.io.read_image(photo2)
+    photo2 = photo2.float().unsqueeze(0).to(config.DEVICE)
+
+    embanding1 = model.forward(photo1)
+    embanding2 = model.forward(photo2)
+
+    dists = [[(e1 - e2).norm().item() for e2 in embanding1] for e1 in embanding2]
+
+    print(dists)
