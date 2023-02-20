cc9da79cb9bbfc298be4060223c803fc4cb3e422
smeyanoff <miradg99@gmail.com>
2023-02-20 14:13:36 +0400

    black isort

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 import pickle
 
-import torch
 import numpy as np
+import torch
 from tqdm import tqdm
 
 from config.config import Config
@@ -17,58 +17,39 @@ res_net = (
     .to(config.DEVICE)
 )
 
-mtcnn = (
-    mtcnn.MTCNN(keep_all=True)
-    .eval()
-    .to(config.DEVICE)
-)
+mtcnn = mtcnn.MTCNN(keep_all=True).eval().to(config.DEVICE)
 
 # extract photos for each person in data folders
 photo_path = config.DATA_PATH
-choose_photos = utils.ChoosePersons(
-    photo_path,
-    persons_size=25,
-    photo_size=15
-)
+choose_photos = utils.ChoosePersons(photo_path, persons_size=25, photo_size=15)
 
 if __name__ == "__main__":
-
     results = {}
     for epoche in tqdm(range(10000)):
-        
-        person1_photo_path, person2_photo_path = (
-            choose_photos.random_choose_person_photos()
-        )
-        
+        (
+            person1_photo_path,
+            person2_photo_path,
+        ) = choose_photos.random_choose_person_photos()
+
         # it doesn't make sense to compare the same photos
         if person1_photo_path == person2_photo_path:
             continue
-        
+
         # read and crop photos
-        person1_photo = utils.read_photo(
-            person1_photo_path
-        )
+        person1_photo = utils.read_photo(person1_photo_path)
         try:
-            cropped_photo1 = utils.cropp_photo(
-                person1_photo, 
-                mtcnn
-            )
+            cropped_photo1 = utils.cropp_photo(person1_photo, mtcnn)
         except TypeError:
             cropped_photo1 = "face not recognized"
 
-        person2_photo = utils.read_photo(
-            person2_photo_path
-        )
+        person2_photo = utils.read_photo(person2_photo_path)
         try:
-            cropped_photo2 = utils.cropp_photo(
-                person2_photo, 
-                mtcnn
-            )
+            cropped_photo2 = utils.cropp_photo(person2_photo, mtcnn)
         except TypeError:
             cropped_photo2 = "face not recognized"
 
         if (
-            cropped_photo1 == "face not recognized" 
+            cropped_photo1 == "face not recognized"
             or cropped_photo2 == "face not recognized"
         ):
             results[epoche] = {
@@ -77,7 +58,7 @@ if __name__ == "__main__":
                 "person_2": person2,
                 "person_2_photo": photo2,
                 "results": "face not recognized",
-                "froad": froad
+                "froad": froad,
             }
 
         # compute embandings
@@ -92,7 +73,7 @@ if __name__ == "__main__":
                     "person_2": person2,
                     "person_2_photo": photo2,
                     "results": "RuntimeError",
-                    "froad": froad
+                    "froad": froad,
                 }
                 continue
             except TypeError:
@@ -102,23 +83,24 @@ if __name__ == "__main__":
                     "person_2": person2,
                     "person_2_photo": photo2,
                     "results": "TypeError",
-                    "froad": froad
+                    "froad": froad,
                 }
                 continue
         results[epoche] = {
-                "person_1": person1,
-                "person_1_photo": photo1,
-                "person_2": person2,
-                "person_2_photo": photo2,
-                "results": {
-                    "euclidean": (
-                        torch.cdist(embanding1, embanding2)     #compute distances
-                        .numpy(force=True)[0][0]
-                    )
-                },
-                "froad": froad
-            }
+            "person_1": person1,
+            "person_1_photo": photo1,
+            "person_2": person2,
+            "person_2_photo": photo2,
+            "results": {
+                "euclidean": (
+                    torch.cdist(embanding1, embanding2).numpy(  # compute distances
+                        force=True
+                    )[0][0]
+                )
+            },
+            "froad": froad,
+        }
 
     # save the experiment results to file
-    with open('data/experiments/res_net_mtcnn_vgg__exp1.pkl', 'wb') as f:
+    with open("data/experiments/res_net_mtcnn_vgg__exp1.pkl", "wb") as f:
         pickle.dump(results, f)
--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,5 @@
-from typing import List
 import os
+from typing import List
 
 import cv2
 import numpy as np
@@ -9,6 +9,7 @@ from config import config
 
 config = config.Config()
 np.random.seed(1)
+
 
 def read_photo(photo_path: str):
     photo = cv2.imread(photo_path)
@@ -39,57 +40,47 @@ def cropp_photo(photo: np.ndarray, face_detect_model) -> torch.tensor:
 
     return cropped_photo
 
-class ChoosePersons():
 
+class ChoosePersons:
     def __init__(
-        self, 
+        self,
         photo_path: str,
         persons_size: int = 25,
         photo_size: int = 15,
-
     ):
-        self.photo_path = photo_path    
+        self.photo_path = photo_path
         # numpy array could't has more 32 dimentions.
         # So if we need to specify probability below /
         # we need to choose not more than 32 persons (size)
         self.persons = np.random.choice(
             os.listdir(self.photo_path),
             size=persons_size,
-            replace=False           # to choose without /
-        )                           # replacement
+            replace=False,  # to choose without /
+        )  # replacement
 
         self.person_photos = {
             person: np.random.choice(
-                os.listdir(
-                    os.path.join(
-                        self.photo_path,
-                        person
-                    )
-                ),
-                size=photo_size
+                os.listdir(os.path.join(self.photo_path, person)), size=photo_size
             )
             for person in self.persons
         }
 
     def random_choose_person_photos(self):
-
         # choose persons for test
         person1 = np.random.choice(self.persons)
-        
+
         # rise the opportunity choosing person1
         # for other persons probability will calculate /
         # as froad probabilyty divide on len persons
-        
+
         proba_persons = [
-            (1 - config.FROAD_PROBA) if x == person1
-            else config.FROAD_PROBA / (len(self.persons)-1)
+            (1 - config.FROAD_PROBA)
+            if x == person1
+            else config.FROAD_PROBA / (len(self.persons) - 1)
             for x in self.persons
         ]
-        
-        person2 = np.random.choice(
-            a=self.persons, 
-            p=proba_persons
-        )
+
+        person2 = np.random.choice(a=self.persons, p=proba_persons)
 
         if person1 == person2:
             froad = 0
@@ -97,30 +88,14 @@ class ChoosePersons():
             froad = 1
 
         # get the pathes for each person
-        person1_path = os.path.join(
-            self.photo_path,
-            person1
-        )
-        person2_path = os.path.join(
-            self.photo_path,
-            person2
-        )
+        person1_path = os.path.join(self.photo_path, person1)
+        person2_path = os.path.join(self.photo_path, person2)
 
         # random choose the photo of each person
-        photo1 = np.random.choice(
-            self.person_photos[person1]      # choose random
-        )
-        photo2 = np.random.choice(
-            self.person_photos[person2]      # choose random
-        )
+        photo1 = np.random.choice(self.person_photos[person1])  # choose random
+        photo2 = np.random.choice(self.person_photos[person2])  # choose random
 
-        person1_photo_path = os.path.join(
-            person1_path,
-            photo1
-        )
-        person2_photo_path = os.path.join(
-            person2_path,
-            photo2
-        )
+        person1_photo_path = os.path.join(person1_path, photo1)
+        person2_photo_path = os.path.join(person2_path, photo2)
 
         return person1_photo_path, person2_photo_path
